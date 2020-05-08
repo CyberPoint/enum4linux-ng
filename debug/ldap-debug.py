d67a9aa5256d26df8e9056924033e6742947e3cf
cddmp <cddmp@users.noreply.github.com>
2020-05-08 08:06:07 +0200

    Update LDAP debug script

--- debug/ldap-debug.py
+++ debug/ldap-debug.py
@@ -3,6 +3,7 @@
 import sys
 import pprint
 from ldap3 import Server, Connection, DSA
+#from ldap3.core.exceptions import LDAPSocketOpenError
 
 def get_namingcontexts(host, tls):
     try:
@@ -11,6 +12,14 @@ def get_namingcontexts(host, tls):
         ldap_con.unbind()
     except Exception as e:
         pprint.PrettyPrinter().pprint(e.args)
+        error = str(e.args[1][0][0])
+        if "]" in error:
+            error = error.split(']', 1)[1]
+        elif ":" in error:
+            error = error.split(':', 1)[1]
+        error = error.lstrip().rstrip()
+
+        print(error)
 
 if not len(sys.argv) > 1:
     print(f"Usage: {sys.argv[0]} host")
@@ -19,4 +28,3 @@ if not len(sys.argv) > 1:
 host = sys.argv[1]
 get_namingcontexts(host,True)
 get_namingcontexts(host,False)
-
